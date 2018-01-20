add2036c6a50fb93e34999fd7a4a44e753d5e079
Sharky! <jdeurt@gmail.com>
2018-01-19 18:42:35 -0600

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ window.onload = function() {
     document.getElementById("formatter").onclick = function() {
         var input = {}, output = {};
         input.element = document.getElementById("input");
-        input.text = input.element.value;
+        input.text = input.element.value.trim();
         output.element = document.getElementById("output");
         
         var char_starting = input.text.charAt(0), char_ending;
@@ -12,6 +12,17 @@ window.onload = function() {
         else if(char_starting === "[")
             char_ending = "]";
         else return;
+        
+        var tabAmount = 0;
+        while(input.text.includes(/^{|^\[/g)) {
+            var done;
+            input.text.match(/{(.*?\n*?.*?)}|\[(.*?\n*?.*?)\]/g).forEach(v => {
+                input.text.replace(v, "");
+            });
+            if(done.length)
+                tabAmount++;
+            console.log(tabAmount);
+        }
         
         output.text = input.text.replace(char_starting, char_starting+"\n\t").replace(new RegExp(char_ending+"$"), "\n"+char_ending);
         output.text = output.text.replace()
